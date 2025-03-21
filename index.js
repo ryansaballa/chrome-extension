de58cfd9c40c3c31ee78abdd2f107c008a754993
perrystudios <73252481+perrystudios@users.noreply.github.com>
2025-03-21 14:14:05 -0400

    import firebase database, initialize app

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
